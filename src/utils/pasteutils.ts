a8b62c2f83ce339fa3323d025cac21cfc3327a14
Lucas I <lucasi@Lucass-MacBook-Pro.local>
2023-05-05 01:05:55 -0400

    add max paste length

--- src/utils/pasteutils.ts
+++ src/utils/pasteutils.ts
@@ -1,5 +1,15 @@
+const MAX_PASTE_LENGTH = 2000;
+
+
 export const sanitizePaste = (content: string): string => {
-    return content.trim();
+
+  let sanitized: string = content.trim();
+
+  if (sanitized.length > MAX_PASTE_LENGTH) {
+    sanitized = sanitized.substring(0, MAX_PASTE_LENGTH);
+  }
+
+  return sanitized;
 }
 
 export const validatePaste = (content: string): boolean => {
